2049faa86b867463e8a741ebabc81f3b5eebbb6c
Admin <Admin@DESKTOP-A79BAK3>
2021-06-28 01:03:28 +0530

    Tested.

---
+++ Proj1/src/test/MainClass.java
@@ -0,0 +1,16 @@
+package test;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		 ClassA obj1 = new ClassA();
+		  ClassB obj2 = new ClassB();
+		   
+		 
+		  
+		  System.out.println("Sum:"+obj1.sum(10, 20));
+		  System.out.println("Sub:"+obj2.sub(10, 10));
+
+	}
+
+}
